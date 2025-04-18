febf3e52c2828b0bcf82b414ed22bea2691df4dc
= <=>
2025-04-18 09:04:19 -0300

    Add media file settings to Django configuration

--- microassembler/settings.py
+++ microassembler/settings.py
@@ -104,6 +104,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media files (Uploaded files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
